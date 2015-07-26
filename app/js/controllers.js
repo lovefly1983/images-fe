b4f3554956285d3dbf0bd7eacc283d143195e08b
chunjie-wang <chunjie.wangcn@gmail.com>
2015-07-26 15:11:16 +0800

    Angular&Bootstrap test

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -10,3 +10,33 @@ MyCtrl1.$inject = [];
 function MyCtrl2() {
 }
 MyCtrl2.$inject = [];
+
+
+function Login() {
+}
+Login.$inject = [];
+
+function Dashboard() {
+
+}
+Dashboard.$inject = [];
+
+function PhoneListCtrl($scope, $http) {
+    $scope.phones = [
+        {"name": "Nexus S",
+            "snippet": "Fast just got faster with Nexus S."},
+        {"name": "Motorola XOOM™ with Wi-Fi",
+            "snippet": "The Next, Next Generation tablet."},
+        {"name": "MOTOROLA XOOM™",
+            "snippet": "The Next, Next Generation tablet."}
+    ];
+    $scope.callIt = function () {
+        $http.get('../comp/status').
+            success(function(data, status, headers, config) {
+                alert("ok");
+            }).
+            error(function(data, status, headers, config) {
+                alert("error");
+            });
+    }
+}
