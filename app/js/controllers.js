4588e34880ce5d0d914c9cc223db05edd7e9ea1d
chunjie-wang <chunjie.wangcn@gmail.com>
2015-08-02 16:07:00 +0800

    AngularTestAddSolrTest

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -30,13 +30,31 @@ function PhoneListCtrl($scope, $http) {
         {"name": "MOTOROLA XOOM™",
             "snippet": "The Next, Next Generation tablet."}
     ];
-    $scope.callIt = function () {
-        $http.get('../comp/status').
+    $scope.search = function() {
+        $http.get('../comp/solr/query'+"?qstr=" + $scope.searchStr +"&maxrows=20").
             success(function(data, status, headers, config) {
-                alert("ok");
+                $scope.docs = data.docs;
             }).
             error(function(data, status, headers, config) {
                 alert("error");
+            });
+    }
+    $scope.addDoc = function() {
+        $http.post('../comp/solr/add', {id : $scope.docToAdd, title : $scope.docToAdd}).
+            success(function(data, status, headers, config) {
+                $scope.status = "add success..."
+            }).
+            error(function(data, status, headers, config) {
+                $scope.status = "add fails..."
+            });
+    }
+    $scope.callIt = function () {
+        $http.get('../comp/status').
+            success(function(data, status, headers, config) {
+                $scope.yourname = data.status;
+            }).
+            error(function(data, status, headers, config) {
+                $scope.yourName = data.status;
             });
     }
 }
